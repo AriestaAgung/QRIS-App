94548955a34a0666b4b07804474f2e4e83cba3e9
Ariesta <ariesta.work@gmail.com>
2024-01-25 00:34:57 +0800

    Remove main storyboard

--- QRIS App/App/AppDelegate.swift
+++ QRIS App/App/AppDelegate.swift
@@ -10,10 +10,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let vc = HomeViewController()
+        let navController = UINavigationController(rootViewController: vc)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
         return true
     }
 
---
+++ QRIS App/Features/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  QRIS App
+//
+//  Created by Ariesta APP on 25/01/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
